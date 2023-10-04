21ac1b18766eed84d0c82a9d80b35c00847677ac
nwright <nathanwrightbusiness@gmail.com>
2023-10-04 18:42:45 -0400

    init

---
+++ content/posts/first.md
@@ -0,0 +1,7 @@
+---
+title: "First"
+date: 2023-08-21T20:19:42-04:00
+draft: false
+---
+
+Test Post
---
+++ default.nix
@@ -0,0 +1,8 @@
+with import <nixpkgs> { };
+stdenv.mkDerivation rec {
+  name = "hugo";
+  env = buildEnv { name = name; paths = buildInputs; };
+  buildInputs = [
+    hugo
+  ];
+}
