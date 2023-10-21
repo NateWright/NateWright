e6685b758f051711bbae274a57010447f5223669
nwright <nathanwrightbusiness@gmail.com>
2023-10-21 12:08:46 -0400

    flake develop

---
+++ default.nix
@@ -0,0 +1,20 @@
+{ stdenv, fetchFromGitHub, hugo }:
+stdenv.mkDerivation {
+  name = "nwright-tech-hugo-site";
+
+  src = self;
+
+  nativeBuildInputs = [ hugo ];
+  buildPhase = ''
+    cp -r $src/* .
+    ${hugo}/bin/hugo
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out
+    cp -r public/* $out/
+    runHook postInstall
+  '';
+}
+
