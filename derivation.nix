e2452d621a9d2b3216e2d19d5d7d7f1a8815ccb8
nwright <nathanwrightbusiness@gmail.com>
2024-01-09 21:35:47 -0500

    update

--- derivation.nix
+++ derivation.nix
@@ -5,8 +5,8 @@ stdenv.mkDerivation {
   src = fetchFromGitHub {
     owner = "NateWright";
     repo = "NateWright";
-    rev = "a24638c468f3a5394d7d8c6aabe9bf2f77486c3b";
-    sha256 = "sha256-SRoIA1TjME7AQH3jzgElfPNSumwwHn2xHH4zZlgRfXI=";
+    rev = "a8b5e69848095dc73f2ef17e6afddb5f6d0456a2";
+    sha256 = "sha256-NuQ1tnuKiIJlp6bHsX4bT89z5uAvfIivT6Sr+Y8uGqg=";
     fetchSubmodules = true;
   };
 
