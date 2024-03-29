0c14603aeef0bc85967a3f962772876a1e13b2cf
nwright <nathanwrightbusiness@gmail.com>
2024-03-29 11:17:33 -0400

    submodule -> flake input

--- derivation.nix
+++ derivation.nix
@@ -1,18 +1,14 @@
-{ stdenv, fetchFromGitHub, hugo }:
+{ stdenv, fetchFromGitHub, hugo, paper-mod }:
 stdenv.mkDerivation {
   name = "nwright-tech-hugo-site";
 
-  src = fetchFromGitHub {
-    owner = "NateWright";
-    repo = "NateWright";
-    rev = "85f2cae0dbbd7bfe257173f0982931e5fadb95ea";
-    sha256 = "sha256-utqLT9z8ErPXnpKOFS7GSKx3TSqIJ++Hj1+xdkHm+v8=";
-    fetchSubmodules = true;
-  };
+  src = ./.;
 
   nativeBuildInputs = [ hugo ];
   buildPhase = ''
     cp -r $src/* .
+    mkdir -p ./themes/PaperMod
+    cp -r ${paper-mod}/* ./themes/PaperMod/
     ${hugo}/bin/hugo
   '';
 
--- flake.nix
+++ flake.nix
@@ -1,16 +1,24 @@
 {
   description = "nwright.tech hugo website";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    paper-mod = {
+      flake = false;
+      url = "github:adityatelange/hugo-PaperMod";
+    };
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, paper-mod }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let pkgs = nixpkgs.legacyPackages.${system}; in
         {
           devShells.default = import ./shell.nix { inherit pkgs; };
           packages = rec {
-            nwright-hugo-website = pkgs.callPackage ./derivation.nix { };
+            nwright-hugo-website = pkgs.callPackage ./derivation.nix {
+              paper-mod = paper-mod;
+            };
             default = nwright-hugo-website;
           };
         }
